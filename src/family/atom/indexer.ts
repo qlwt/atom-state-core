0406995d48964ad06ad2bbcb0f1c4aa5e9169765
LerpanWortt <lerpanwortt@gmail.com>
2025-10-18 17:40:43 +0300

    fix atomfamily_new_indexer not using extracted constructor

--- src/family/atom/indexer.ts
+++ src/family/atom/indexer.ts
@@ -1,8 +1,9 @@
 import { atomfamily_new } from "#src/family/atom/index.js"
+import { family_new } from "#src/family/new/index.js"
 import type { AtomFamily } from "#src/family/type/AtomFamily.js"
+import type { Indexer } from "#src/indexer/type/indexer.js"
 import type { AtomSelectorStatic } from "#src/selector/type/AtomSelector.js"
 import { atomvalue_new } from "#src/value/atom/index.js"
-import type { Indexer } from "#src/indexer/type/indexer.js"
 import type * as sc from "@qyu/signal-core"
 
 export type AtomFamily_NewIndexer_Params<Ref, Data, Filter, Param = Filter> = Readonly<{
@@ -20,6 +21,28 @@ export const atomfamily_new_indexer = function <Ref, Data, Filter, Param = Filte
         const indexer = params.indexer()
 
         reg(params.connect(indexer))
+
+        return family_new({
+            key: params.key ?? JSON.stringify.bind(JSON),
+
+            get: (param, cache) => {
+                if (params.param) {
+                    const watcher = indexer.watcher_new(params.param(param))
+
+                    cache(watcher)
+
+                    return watcher
+                }
+
+                {
+                    const watcher = indexer.watcher_new(param as any as Filter)
+
+                    cache(watcher)
+
+                    return watcher
+                }
+            }
+        })
 
         return reg(atomfamily_new({
             key: params.key ?? JSON.stringify.bind(JSON),
