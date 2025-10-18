13d1ed07f11d9d9efc7e89a56a2448b5d1919220
LerpanWortt <lerpanwortt@gmail.com>
2025-10-18 17:21:50 +0300

    Fix atomremreq_new not using extracted constructor.

--- src/remreq/atom/index.ts
+++ src/remreq/atom/index.ts
@@ -1,53 +1,9 @@
-import type { AtomRemReq, AtomRemReq_State } from "#src/remreq/type/State.js"
+import { remreq_new } from "#src/remreq/new/index.js"
+import type { AtomRemReq } from "#src/remreq/type/State.js"
 import { atomvalue_new } from "#src/value/atom/index.js"
-import * as sc from "@qyu/signal-core"
 
 export const atomremreq_new = function <Data>(): AtomRemReq<Data> {
     return atomvalue_new(() => {
-        const state = sc.signal_new_value<AtomRemReq_State<Data> | null>(null)
-
-        return {
-            input: (message: AtomRemReq_State<Data> | null) => {
-                state.output()?.abort()
-
-                let interrupted = false
-
-                if (message) {
-                    message.promise.then(() => {
-                        if (interrupted) { return }
-
-                        state.input(null)
-                    }, () => {
-                        if (interrupted) { return }
-
-                        state.input(null)
-                    })
-
-                    state.input({
-                        ...message,
-
-                        abort: () => {
-                            interrupted = true
-
-                            message.abort()
-                        }
-                    })
-                } else {
-                    state.input(null)
-                }
-            },
-
-            output: () => {
-                return state.output()
-            },
-
-            rmsub: sub => {
-                state.rmsub(sub)
-            },
-
-            addsub: (sub, config) => {
-                state.addsub(sub, config)
-            },
-        }
+        return remreq_new()
     })
 }
