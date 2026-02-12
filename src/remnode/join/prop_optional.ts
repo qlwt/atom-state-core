152b50c510cedf74a458fe44a10b709d13c093f8
LerpanWortt <lerpanwortt@gmail.com>
2026-02-12 12:11:08 +0100

    fix wrong import

--- src/remnode/join/prop_optional.ts
+++ src/remnode/join/prop_optional.ts
@@ -1,14 +1,14 @@
-import * as asc from "@qyu/atom-state-core"
+import type { AtomRemNode_Join_Factory } from "#src/remnode/type/Join.js"
 import * as sc from "@qyu/signal-core"
 
 export type AtomRemNode_Join_Prop_Optional_Params<Param, TParam, Result> = {
     readonly transformer: (param: Param) => TParam | undefined
-    readonly source: asc.AtomRemNode_Join_Factory<TParam, Result>
+    readonly source: AtomRemNode_Join_Factory<TParam, Result>
 }
 
 export const atomremnode_join_prop_optional = function <Param, TParam, Result>(
     params: AtomRemNode_Join_Prop_Optional_Params<Param, TParam, Result>
-): asc.AtomRemNode_Join_Factory<sc.OSignal<Param | null>, Result | undefined> {
+): AtomRemNode_Join_Factory<sc.OSignal<Param | null>, Result | undefined> {
     return ({ reg }) => {
         return key => {
             return sc.osignal_new_memo(sc.osignal_new_pipeflat(key, key_o => {
